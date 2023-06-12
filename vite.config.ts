7ace86926557ca2ec2f8f07fa9ca424ef7cd0e09
Tigran <tgpett@gmail.com>
2023-06-12 12:14:06 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [react(), dts({ rollupTypes: true })],
+
+  build: {
+    lib: {
+      entry: ['src/index.ts'],
+      formats: ['es'],
+      fileName: 'index',
+    },
+  },
+});
